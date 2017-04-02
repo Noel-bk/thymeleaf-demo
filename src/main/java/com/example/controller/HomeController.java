f2e345f996102214607bc49a41169c51023fcbe5
Noel.b <jaeyeong.b@gmail.com>
2017-04-02 20:29:39 +0900

    Add home controller and view

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,25 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+/**
+ * Created by Noel on 4/1/17.
+ */
+@Controller
+public class HomeController {
+
+    @GetMapping(value = "/")
+    public String home(Model model) {
+        DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, Locale.getDefault());
+        model.addAttribute("message", "YOLO!");
+        model.addAttribute("serverTime", dateFormat.format(new Date()));
+        return "home";
+    }
+}
